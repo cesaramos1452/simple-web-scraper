e3cf252d85b2c64c7529d39608e3f39050851696
Cesar Ramos <cesaramos1452@yahoo.com>
2021-01-02 16:39:07 -0800

    fixed issues the bug was I forgot the headers for the Kenzie website

--- scrape.py
+++ scrape.py
@@ -4,18 +4,25 @@ import re
 import sys
 
 """SIMPLE WEB SCRAPER BY CESAR RAMOS"""
+head_1 = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
+head_2 = '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+headers = {
+    'User-Agent': head_1 + head_2}
+# t = requests.get('https://www.kenzie.academy', headers=headers).text
+# print(t)
 
 
 def url(site):
     """"returns all urls from user's input site"""
-    url_pattern = (r"""\bhttp[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|
-    [!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\b""")
+    url_pattern = (
+        r"""http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F]
+        [0-9a-fA-F]))+""")
     return re.findall(url_pattern, site)
 
 
 def email(site):
     """"returns all emails from user's input site"""
-    email_pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$)"
+    email_pattern = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
     return (re.findall(email_pattern, site) if
             re.findall(email_pattern, site) else None)
 
@@ -47,13 +54,13 @@ def main(args):
     parser = create_parser()
     ns = parser.parse_args(args)
     if ns.website:
-        site = requests.get(ns.website).text
+        site = requests.get(ns.website, headers=headers).text
         if url(site):
-            print('URLS: ', '\n'.join(url(site)))
+            print('URLS: ', '\n'.join(set(url(site))))
         else:
             print('URLS: ', None)
         if email(site):
-            print('EMAILS: ', '\n'.join(email(site)))
+            print('EMAILS: ', '\n'.join(set(email(site))))
         else:
             print('EMAILS: ', None)
         if phone(site):
